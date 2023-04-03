003c9eda457f1c01b53c759c18245ef97d8821e2
cesarorozco9628 <paulocesarorozcoguerrero@hotmail.com>
2023-04-03 16:33:45 -0600

    preparacion lola prod

--- js/main.js
+++ js/main.js
@@ -5,20 +5,6 @@ function hash_text(text){
      return myPassword(text)
 }
 
-const close_modal_mgn = (id) => {
-    const modal_mgn = document.getElementById(id);  
-    modal_mgn.style.display = 'none';
-}
-
-const open_modal_mgn = (id) => {
-    const modal_mgn = document.getElementById(id);
-    modal_mgn.style.display = 'flex'
-}
-
-
-const handle_modal_width = (id) => {
-
-}
 
 const cipher = salt => {
     const textToChars = text => text.split('').map(c => c.charCodeAt(0));
@@ -65,52 +51,4 @@ const put_params_bulding = (data) => {
     </div>
     <iframe class="${width_iframe}" src="https://uat.morgana.mx/lola/registra_cotiza/${params.length>0 ? `?${params}`:''}" id="id_ifm_mgn" width=""></iframe>
     `
-    open_modal_mgn(id);handle_modal_width(id);
-}
-
-const get_params_bulding = () => {
-    let url = window.location.href;
-    let params = url.split('?').length > 1 ? url.split('?')[1] : url;
-    if( params !== url){
-        let mydescrip = decipher('morganamx');
-        params = mydescrip(params);
-        injection_data(params);
-        return   
-    }
-}
-
-const injection_data = (params) => {
-    const urlParams = new URLSearchParams(params); const precio = urlParams.get('price'); const ubicacion = urlParams.get('ubication');
-    const email = urlParams.get('email');const name = urlParams.get('name');const phone = urlParams.get('phone');
-      
-  if(document.getElementById('id_valor') ){
-    document.getElementById('id_valor').value = precio !== null ? parseInt(precio).toLocaleString('MXN') : '';
-  }
-  if(document.getElementById('id_ubication')){
-    document.getElementById('id_ubication').value = ubicacion !== null ? ubicacion : '9';
-  }
-
-}
-
-window.addEventListener("load", (event) => {
-    get_params_bulding();
-  });
-
-
-
-  const handle_link_oferts = () => {
-    let url = window.location.href = './oferts_request.html'
-  }
-
-function switch_logged (e){
-    const {value} = e; 
-    let logged = document.querySelector('#id_div_logged');
-    let not_logged = document.querySelector('#id_div_not_logged');
-    if( value === 'si' ){
-        logged.classList.remove('d-none');
-        not_logged.classList.add('d-none');
-    }else{
-        logged.classList.add('d-none');
-        not_logged.classList.remove('d-none');
-    }
 }
