3304b8677e59ea42e9ba07ebbee409e1ac4a96b0
cesarorozco9628 <paulocesarorozcoguerrero@hotmail.com>
2023-04-03 09:59:31 -0600

    responsive

--- js/main.js
+++ js/main.js
@@ -44,15 +44,26 @@ const decipher = salt => {
 
 const put_params_bulding = (data) => {
     let { username ,id, price, ubication, email, name, phone } = data;
-
+    let width_container = '';
+    let width_iframe = '';
     let div_ifrm = document.getElementById(id);
     let params = `${username?`username=${username}`:''}${price?`&price=${price}`:''}${ubication?`&ubication=${ubication}`:''}${email?`&email=${email}`:''}${name?`&name=${name}`:''}${phone?`&phone=${phone}`:''}`;
     params = hash_text(params);
+
+    if (/Mobile/i.test(navigator.userAgent)) {
+      width_container = 'div-responsive'
+      width_iframe = 'iframe-responsive'
+    } else {
+      width_container = 'div-width'
+      width_iframe = 'iframe-width'
+      // El código se está ejecutando en un dispositivo de escritorio
+    }
+
     div_ifrm.innerHTML = `
-    <div class="d-flex justify-content-end pb-2" id="id_container_close">
+    <div class="d-flex justify-content-end pb-2 ${width_container}" id="id_container_close">
         <img src="./img/ICONO_CLOSE.svg" class="cursor-pointer" onclick="close_modal_mgn('id_fiv_frm_mgn')">
     </div>
-    <iframe class="" src="https://uat.morgana.mx/lola/registra_cotiza/${params.length>0 ? `?${params}`:''}" id="id_ifm_mgn" width=""></iframe>
+    <iframe class="${width_iframe}" src="https://uat.morgana.mx/lola/registra_cotiza/${params.length>0 ? `?${params}`:''}" id="id_ifm_mgn" width=""></iframe>
     `
     open_modal_mgn(id);handle_modal_width(id);
 }
